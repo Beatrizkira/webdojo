5148b016c3770e1af288d57f8d6628366924d244
Beatriz <yunogasaio@outlook.com>
2026-07-25 15:40:49 -0300

    add Cypress configuration and initial test setup

---
+++ web/cypress/e2e/login.cy.js
@@ -0,0 +1,13 @@
+describe('template spec', () => {
+  it('passes', () => {
+    cy.viewport(1920,1080)
+    cy.visit('http://localhost:3000')
+  })
+})
+
+describe('Login', ()=>{
+  it ('Deve logar com sucesso', ()=> {
+    cy.viewport(1440,900)
+    cy.visit('http://localhost:3000')
+  })
+})
