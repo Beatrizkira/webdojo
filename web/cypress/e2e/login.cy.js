bf2758ed5f5bef61d55f289da7313e59a58935e3
Beatriz <yunogasaio@outlook.com>
2026-08-01 21:31:41 -0300

    implement login tests and custom commands for Cypress

--- web/cypress/e2e/login.cy.js
+++ web/cypress/e2e/login.cy.js
@@ -1,16 +1,41 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.viewport(1920,1080)
-    cy.visit('http://localhost:3000')
+describe('Login', ()=>{
+  it ('Deve logar com sucesso', ()=> {
+    cy.start()
+    cy.submitLoginForm('papito@webdojo.com', 'katana123')
+
+//Não é legal utilizar classes de utilização , IDs são excelentes propriedades para utliza na automação de testes,
+//Copiar Xpath de inspencionar nãe é legal, o ideal é montar um Xpath// 
+// Cypress não tem suporte nativo para Xpath//
+
+
+    cy.get('[data-cy="user-name"]')
+     .should('be.visible')
+     //Should é uma espectativa, ele mostra que precisa estar visivel//
+  // A gente pode pedir para o dev escrever o data.cy no código para criar uma propriedade para facilitar a automação de testes//
+   .and('have.text', 'Fernando Papito')
+
+   cy.get('[data-cy="welcome-message"]')
+   .should('be.visible')
+   .and('have.text', 'Olá QA, esse é o seu Dojo para aprender Automação de Testes.')
   })
 })
 
 describe('Login', ()=>{
-  it ('Deve logar com sucesso', ()=> {
-    cy.viewport(1440,900)
-    cy.visit('http://localhost:3000')
+  it ('Não deve logar com senha inválida', ()=> {
+    cy.start()
+    cy.submitLoginForm('papito@webdojo.com', 'katana32')
+ 
+    cy.contains('Acesso negado! Tente novamente.')
+     .should('be.visible')
+  })   
+})
 
-    cy.get('#email').type('papito@webdojo.com')
-    cy.get('#password').type('katana123')
-  })
+describe('Login', ()=>{
+  it ('Não deve logar com e-mail inválido', ()=> {
+    cy.start()
+    cy.submitLoginForm('404@webdojo.com', 'katana123')
+
+    cy.contains('Acesso negado! Tente novamente.')
+     .should('be.visible')
+  })   
 })
