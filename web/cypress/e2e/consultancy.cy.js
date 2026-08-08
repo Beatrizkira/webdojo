b8e2af2668b57eaf87e194a85d118555702b799f
Beatriz <yunogasaio@outlook.com>
2026-08-08 10:33:46 -0300

    refactor consultancy form test to use placeholder selectors for input fields

--- web/cypress/e2e/consultancy.cy.js
+++ web/cypress/e2e/consultancy.cy.js
@@ -6,7 +6,16 @@ describe('Formulário de Consultoria', () => {
 
         cy.goTo('Formulários', 'Consultoria')
 
-        cy.get('#name').type('Fernando Papito')
+        cy.get('input[placeholder="Digite seu nome completo"]').type('Fernando Papito')
+        cy.get('input[placeholder="Digite seu email"]').type('papito@teste.com.br')
+        cy.get('input[placeholder="(00) 00000-0000"]')
+        .type('(11) 99999-9999')
+        .should('have.value', '(11) 99999-9999')
+
+        cy.contains('label','Tipo de Consultoria')
+         .parent()
+         .find('select')
+         .select('Individual')
     })
 })
 
