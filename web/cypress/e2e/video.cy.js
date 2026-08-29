5cd4e3d89de39d9c01fc90398906b5cf452dc7c1
Beatriz <yunogasaio@outlook.com>
2026-08-29 20:16:52 -0300

    Add chat component styles to index-Dh1eSc5b.css for improved UI/UX

---
+++ web/cypress/e2e/video.cy.js
@@ -0,0 +1,26 @@
+describe('Tocar o vídeo', () => {
+    
+    it('Deve poder tocar o vídeo de exemplo', () => {
+        cy.start()
+        cy.submitLoginForm('papito@webdojo.com', 'katana123')
+        cy.contains('Video').click()
+
+        cy.get('iframe[title="Video Player"]')
+         .should('exist')
+         .its('0.contentDocument.body')
+         .then(cy.wrap)
+         .as('iFramePlayer')
+
+        cy.get('@iFramePlayer')
+          .find('button')
+          .first()
+          .click()
+
+        cy.get('@iFramePlayer')
+          .find('.pause-button')
+          .should('be.visible')
+        
+          
+
+    })
+})
